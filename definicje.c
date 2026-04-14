9bfa66b328820b1d8fc039085e5c814ced356346
janorlinski <91850084+janorlinski@users.noreply.github.com>
2026-04-14 10:57:04 +0200

    Iloczyn i iloraz

--- definicje.c
+++ definicje.c
@@ -1,7 +1,15 @@
 double suma(double a, double b) {
 	return a+b;
-	}
+}
 
 double roznica(double a, double b) {
 	return a-b;
-	}
+}
+
+double iloczyn (duble a, double b) {
+	return a*b;
+}
+
+double iloraz (double a, double b) {
+	return a/b;
+}
