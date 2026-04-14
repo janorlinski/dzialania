473e89e2167349d8b6fadb5076c716763937bfeb
janorlinski <91850084+janorlinski@users.noreply.github.com>
2026-04-14 11:01:29 +0200

    Fix missing std

--- main.c
+++ main.c
@@ -6,10 +6,10 @@ int main () {
   double a = 7.2;
   double b = 2.19;
   
-  std::cout << "Test dodawania: " << a << " + " << b << " = " << suma(a, b) << endl;  
-  std::cout << "Test odejmowania: " << a << " - " << b << " = " << roznica(a, b) << endl;  
-  std::cout << "Test mnozenia: " << a << " * " << b << " = " << iloczyn(a, b) << endl;  
-  std::cout << "Test dzielenia: " << a << " / " << b << " = " << iloraz(a, b) << endl;
+  std::cout << "Test dodawania: " << a << " + " << b << " = " << suma(a, b) << std::endl;  
+  std::cout << "Test odejmowania: " << a << " - " << b << " = " << roznica(a, b) << std::endl;  
+  std::cout << "Test mnozenia: " << a << " * " << b << " = " << iloczyn(a, b) << std::endl;  
+  std::cout << "Test dzielenia: " << a << " / " << b << " = " << iloraz(a, b) << std::endl;
 
   return 0;
 
